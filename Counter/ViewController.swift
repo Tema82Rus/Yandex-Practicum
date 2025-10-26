e8d5bdd546f472307726375a8dc2f6820a0c836c
Tema82Rus <Fenix23245@yandex.ru>
2025-10-27 00:41:50 +0300

    Добавлены кнопки +, -, сбросить

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -8,21 +8,57 @@
 import UIKit
 
 class ViewController: UIViewController {
+    //переменная для хранения счёта нажатий
+    var count: Int = 0
     
-    //связь между лейблом
+    //переменная для хранения истории изменений
+    var history: UITextView?
+    
+    //связь между текстовым полем "История изменений:"
+    @IBOutlet weak var textHistory: UITextView!
+    
+    //связь между лейблом "Значение счётчика:"
     @IBOutlet weak var counterLabel: UILabel!
     
-    //связь между кнопкой
+    //связь между кнопкой "+"
+    @IBOutlet weak var buttonIncrement: UIButton!
+    
+    //связь между кнопкой "-"
+    @IBOutlet weak var buttonDecrement: UIButton!
+    
+    //связь между кнопкой "Обнулить"
+    @IBOutlet weak var buttonReset: UIButton!
+    
+    //связь между кнопкой посчитать
     @IBOutlet weak var changeButton: UIButton!
     
-    //нажатие на кнопку
+    //метод увеличить счёт
+    @IBAction func buttonIncrementTap(_ sender: Any) {
+        count += 1
+        counterLabel.text = "Значение счётчика: \(count)"
+        history = "значение изменено на +1"
+        textHistory = history
+    }
+    
+    //метод уменьшить счёт
+    @IBAction func buttonDecrementTap(_ sender: Any) {
+        if !(count == 0) {
+            count -= 1
+            counterLabel.text = "Значение счётчика: \(count)"
+        } else { count = 0 }
+    }
+    
+    //метод обнулить счёт
+    @IBAction func buttonResetTap(_ sender: Any) {
+        count = 0
+        counterLabel.text = "Значение счётчика: \(count)"
+    }
+    
+    //метод нажатие на кнопку
     @IBAction func buttonDidTap(_ sender: Any) {
         count += 1
         counterLabel.text = "Значение счётчика: \(count)"
     }
-    
-    //переменная для счёта нажатий
-    var count: Int = 0
     
     //view создано и готово к показу
     override func viewDidLoad() {
